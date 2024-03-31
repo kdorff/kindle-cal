658ee3053d7bf215d09787e0159f991add861aaa
Kevin Dorff <kevin@dorffweb.com>
2024-03-31 01:00:10 -0500

    Improve code. Improve png pipeline, removing pngjs. No files created for PNG process.

--- src/app/png/route.ts
+++ src/app/png/route.ts
@@ -1,59 +1,66 @@
-import fs from "fs";
 import { chromium } from "playwright";
 import sharp from "sharp";
-// @ts-ignore
-import { PNG } from "pngjs/browser";
 
-export async function GET() {
-  // Take the screenshot
-  console.log("Launching chromium");
-  const browser = await chromium.launch();
-  console.log("Opening page");
-  const page = await browser.newPage();
-  console.log("Setting viewport size");
-  await page.setViewportSize({ width: 800, height: 600 });
-  console.log("Requesting URL");
-  await page.goto("http://localhost:3000");
-  console.log("Taking screenshot");
-  const imageBuffer = await page.screenshot();
+/**
+ * Take a screenshot of a web page.
+ * Return as a png image in a Buffer
+ * @param url URL to screenshot
+ * @returns Buffer for png image
+ */
+async function grabScreenshot(url: string): Promise<Buffer> {
+  let browser = undefined;
+  let page = undefined;
+  const imageBuffer = undefined;
+  try {
+    console.log("Launching chromium");
+    browser = await chromium.launch();
+    console.log("Opening page");
+    page = await browser.newPage();
+    console.log("Setting viewport size");
+    await page.setViewportSize({ width: 800, height: 600 });
+    console.log("Requesting URL");
+    await page.goto("http://localhost:3000");
+    console.log("Taking screenshot");
+    return await page.screenshot();
+  } finally {
+    // Close the browser
+    page && page.close();
+    browser && browser.close();
+  }
+}
 
-  // Close the browser
-  page.close();
-  browser.close();
-
-  // Rotate the image.
-  console.log("Rating the image");
-  const rotatedImageBuffer = await sharp(imageBuffer)
+/**
+ * Transform the png image in imageBuffer
+ * by rotating it 90 degreesleft and converting it to grayscale.
+ * Return as a png image in a Buffer
+ * @param imageBuffer png image to transform
+ * @returns Buffer for transformed png image
+ */
+async function transformImage(
+  imageBuffer: Buffer
+): Promise<Buffer | undefined> {
+  let rotatedGrayscaleImage = undefined;
+  console.log("Transforming image");
+  await sharp(imageBuffer)
     .rotate(-90)
-    // .toBuffer()
-    .toFile("/tmp/test-color.png")
+    .grayscale()
+    .toBuffer()
     .then((info) => {
-      console.log(info);
+      console.log("Transform implage completed", info);
+      rotatedGrayscaleImage = info;
     })
     .catch((err) => {
-      console.log(err);
+      console.log("Transform implage FAILED", err);
     });
-  console.log("Rotated image written");
+  return rotatedGrayscaleImage;
+}
 
-  // Grayscale the buffer
-  console.log("Grayscale, 4 bit the PNG");
+export async function GET() {
+  const imageBuffer = await grabScreenshot("http://localhost:3000");
+  const transformedImage = await transformImage(imageBuffer);
 
-  const png = new PNG({
-    width: 600,
-    height: 800,
-    colorType: 0,
-  });
-
-  const src = fs.createReadStream("/tmp/test-color.png");
-  const dst = fs.createWriteStream("/tmp/test-gray.png");
-  png.on("parsed", function () {
-    png.pack().pipe(dst);
-  });
-  await src.pipe(png);
-
-  // Send it as a response
-  console.log("Returning result");
-  const response = new Response(fs.readFileSync("/tmp/test-gray.png"));
+  console.log("Returning png", transformedImage);
+  const response = new Response(transformedImage);
   response.headers.set("content-type", "image/png");
   response.headers.set("Cache-Control", "no-store");
   return response;
